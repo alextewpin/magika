ff2bc4fa7c0c455d7fb9d6bb291887864907179a
Alexander Tewpin <alex.tewpin@gmail.com>
2016-04-18 00:20:06 +0300

    Path update

--- src/components/_utils/fetchData.js
+++ src/components/_utils/fetchData.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 export default function fetchData (onSuccess) {
   axios.all([
-    axios.get('/data-bestiary.json'),
-    axios.get('/data-classes.json'),
-    axios.get('/data-spellbook.json')
+    axios.get('data-bestiary.json'),
+    axios.get('data-classes.json'),
+    axios.get('data-spellbook.json')
   ])
   .then(res => {
     onSuccess(res.reduce((prev, current) => {
